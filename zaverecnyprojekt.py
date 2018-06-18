4655732bf1aec8bdfa8f8ee2f7a1a49aa8f2b2a3
Skurkys <31956765+Skurkys@users.noreply.github.com>
2018-06-18 18:26:58 +0200

    verze 2

--- zaverecnyprojekt.py
+++ zaverecnyprojekt.py
@@ -12,13 +12,13 @@ from pyglet import gl
 sirka = 900
 vyska = 600
 
-velmic = 20
-sirpalka = 10
-delpalka = 100
+velikostmice= 20
+sirkapalky = 10
+delkapalky = 100
 rychlost = 200
 rychpalka = rychlost * 1.5
 
-delkapulcary = 20
+delkacarky = 20
 velfont = 40
 odsazenitextu = 30
 
@@ -38,6 +38,39 @@ def nakresli_obdelnik(x1, y1, x2, y2):
     gl.glVertex2f(int(x2), int(y1))  # vrchol D, nakresli trojuhelnik BCD
     gl.glEnd()  # ukonci kresleni trojuhelniku
 
+def vykresli():
+    gl.glClear(gl.GL_COLOR_BUFFER_BIT)  # smaz obsah okna (vybarvi na cerno)
+    gl.glColor3f(1, 1, 1)  # nastav barvu kresleni na bilou
 
+window = pyglet.window.Window(width=sirka, height=vyska)
+window.push_handlers(
+    on_draw=vykresli,
+)
+
+def vykresli():
+    nakresli_obdelnik(
+        pozice_mice[0] - velikostmice // 2,
+        pozice_mice[1] - velikostmice // 2,
+        pozice_mice[0] + velikostmice // 2,
+        pozice_mice[1] + velikostmice // 2,
+    )
+    
+def vykresli():
+    for x, y in [(0, pozice_palek[0]), (sirka, pozice_palek[1])]:
+        nakresli_obdelnik(
+            x - sirkapalky,
+            y - delkapalky // 2,
+            x + sirkapalky,
+            y + delkapalky // 2,
+        )
+        
+def vykresli():
+    for y in range(delkacarky // 2, vyska, delkacarky * 2):
+        nakresli_obdelnik(
+            sirka // 2 - 1,
+            y,
+            sirka // 2 + 1,
+            y + delkacarky
+        )
 okno = pg.window.Window(width = sirka, height = vyska)
 pg.app.run()
